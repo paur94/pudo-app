5a80ff900662a3f7967c3099d8ecdae8f7ff4041
paur94 <paruyryeprikyan9@gmail.com>
2022-06-27 10:31:54 +0400

    First step

--- routes.js
+++ routes.js
@@ -1,42 +1,44 @@
-const express = require('express');
-
-
+const express = require("express");
+const fetch = require( "node-fetch");
 const router = express.Router();
+require('dotenv').config()
 
-router.get("/", function(req, res) {
+const { PUDO_URL, PUDO_CODE, PUDO_PASSWORD } = process.env;
+
+router.get("/", function (req, res) {
   res.send("Welcome to the Webhooks API");
 });
-
-router.post("/log-shopify-webhook", async function(req, res) {
-  //console.log(req.body);
-
+console.log(PUDO_CODE)
+router.post("/fulfillment_webhook", async function (req, res) {
   const payload = req.body;
-  //console.log(payload);
 
-  let webhook_info = {
-    webhook_id : req.headers['x-shopify-webhook-id'],
-    total_items : payload.line_items.length,
-    time : payload.created_at
+  if (!payload.tracking_number) {
+    res.status(201).send({
+      message: "Webhook Event successfully logged",
+    });
+    return;
   }
 
-  const save_webhook = await req.db
-  .collection("webhooks")
-  .insertOne(webhook_info);
+  const pudo_req_data = {
+    partnerCode: PUDO_CODE,
+    partnerPassword: PUDO_PASSWORD,
+    trackingNumber: payload.tracking_number,
+    memberMobile: payload.destination.phone,
+    memberEmail: payload.email,
+  };
+  const pudo_response = await fetch(PUDO_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(pudo_req_data),
+  });
+  const pudo_data = await pudo_response.json()
+  console.log(pudo_data);
 
   res.status(201).send({
-    message: "Webhook Event successfully logged"
+    message: "Webhook Event successfully logged",
   });
-});
-
-router.get("/fetch-webhooks-logs", async function(req, res) {
-  console.log(req.body);
-  
-  const webhooks = await req.db
-  .collection("webhooks")
-  .find()
-  .toArray();
-
-  res.status(200).send(webhooks);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,10 @@
 const express = require("express");
 const routes = require("./routes");
 const bodyParser = require('body-parser');
-const { startDatabase } = require("./database");
-const crypto = require('crypto');
-
+require('dotenv').config();
 // App
+console.log(process.env.PUDO_URL)
 const app = express();
-
-const sigHeaderName = 'x-shopify-hmac-sha256';
-const sigHashAlg = 'sha256';
-const secret = "xx-xx-x";
-
 
 app.use(bodyParser.json(
     {
@@ -27,45 +21,6 @@ app.use(bodyParser.json(
 const port = process.env.PORT || "1337";
 app.set("port", port);
 
-//Database Setup
-const dbSetup = async (req, res, next) => {
-    if (!req.db) {
-        const db = await startDatabase();
-        req.db = db;
-    }
-
-    next();
-};
-
-app.use(dbSetup);
-
-//Validate payload
-/* function validatePayload(req, res, next) {
-
-    if(req.method == "POST"){
-        if (!req.rawBody) {
-            return next('Request body empty')
-        }
-
-        const body = req.rawBody;
-        const hmacHeader = req.get(sigHeaderName);
-        //Create a hash based on the parsed body
-        const hash = crypto
-            .createHmac(sigHashAlg, secret)
-            .update(body, "utf8", "hex")
-            .digest("base64");
-
-        // Compare the created hash with the value of the X-Shopify-Hmac-Sha256 Header
-        if (hash !== hmacHeader) {
-            return next(`Request body digest (${hash}) did not match ${sigHeaderName} (${hmacHeader})`)
-        } 
-
-    }
-
-    return next()
-
-}
-app.use(validatePayload); */
 app.use('/', routes);
 
 // Server
