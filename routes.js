f1e2d9d37b52dfb6e174883e99acff784f8f8875
paur94 <paruyryeprikyan9@gmail.com>
2022-06-30 09:19:49 +0400

    make it better

--- routes.js
+++ routes.js
@@ -11,7 +11,7 @@ router.get("/", function (req, res) {
 
 router.post("/fulfillment_webhook", async function (req, res) {
   const payload = req.body;
-console.log(payload, 'payload')
+  console.log(payload, 'payload')
   if (!payload.tracking_number) {
     res.status(200).send({
       message: "Webhook Event successfully logged",
