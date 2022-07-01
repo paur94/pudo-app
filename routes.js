b61456be10995d37b3c2255dfff49f2814a8c917
paur94 <paruyryeprikyan9@gmail.com>
2022-07-01 15:27:19 +0400

    Separated update and create

--- routes.js
+++ routes.js
@@ -9,9 +9,49 @@ router.get("/", function (req, res) {
   res.send("Welcome to the Webhooks API");
 });
 
-router.post("/fulfillment_webhook", async function (req, res) {
+router.post("/fulfillment_created_webhook", async function (req, res) {
   const payload = req.body;
-  console.log("order_id", payload.order_id);
+  console.log("created_order_id", payload.order_id);
+  if (
+    !(
+      payload.tracking_number &&
+      payload.destination?.company?.indexOf("PUDO") > -1
+    )
+  ) {
+    res.status(200).send({
+      message: "Webhook Event successfully logged",
+    });
+    return;
+  }
+
+  const pudo_req_data = {
+    partnerCode: PUDO_CODE,
+    partnerPassword: PUDO_PASSWORD,
+    trackingNumber: "1234",
+    memberEmail: payload.email,
+    memberName: payload.destination.name,
+    memberPhone: payload.destination.phone,
+  };
+
+  const pudo_response = await fetch(PUDO_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(pudo_req_data),
+  });
+  const pudo_data = await pudo_response.json();
+  console.log(pudo_data);
+
+  res.status(200).send({
+    message: "Webhook Event successfully logged",
+  });
+});
+
+
+router.post("/fulfillment_updated_webhook", async function (req, res) {
+  const payload = req.body;
+  console.log("updated_order_id", payload.order_id);
   if (
     !(
       payload.tracking_number &&
