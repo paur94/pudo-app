325a6b991e6cda96e8778efbe62d267f935684ff
paur94 <paruyryeprikyan9@gmail.com>
2022-10-06 11:02:56 +0400

    cors-4

--- server.js
+++ server.js
@@ -1,46 +1,62 @@
 const express = require("express");
-const cors = require('cors')
+const cors = require("cors");
 
 const routes = require("./routes");
-const bodyParser = require('body-parser');
-require('dotenv').config();
+const bodyParser = require("body-parser");
+require("dotenv").config();
 const mongoose = require("mongoose");
 // App
-console.log(process.env.PUDO_URL)
+console.log(process.env.PUDO_URL);
 const app = express();
-app.use(cors({ origin: "/pd_shipment_status", credentials: true }))
+app.use(cors());
 //mongoose
 app.use(express.json());
-mongoose.connect(process.env.MONGO_CONNECTION,
-    err => {
-        if(err) throw err;
-        console.log('connected to MongoDB')
-    });
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested, Content-Type, Accept Authorization"
+    );
+    if (req.method === "OPTIONS") {
+        res.header(
+            "Access-Control-Allow-Methods",
+            "POST, PUT, PATCH, GET, DELETE"
+        );
+        return res.status(200).json({});
+    }
+    next();
+});
+
+mongoose.connect(process.env.MONGO_CONNECTION, (err) => {
+    if (err) throw err;
+    console.log("connected to MongoDB");
+});
 
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
-  console.log("Connected successfully");
+    console.log("Connected successfully");
 });
 
 app.use(routes);
 
-app.use(bodyParser.json(
-    {
+app.use(
+    bodyParser.json({
         verify: (req, res, buf, encoding) => {
             if (buf && buf.length) {
-                req.rawBody = buf.toString(encoding || 'utf8');
+                req.rawBody = buf.toString(encoding || "utf8");
             }
         },
-    }
-));
+    })
+);
 
 // Set port
 const port = process.env.PORT || "1337";
 app.set("port", port);
 
-app.use('/', routes);
+app.use("/", routes);
 
 // Server
 app.listen(port, () => console.log(`Server running on localhost:${port}`));
