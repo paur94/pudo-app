10f2e23945fde21492c4af601812d1aa797cb506
paur94 <paruyryeprikyan9@gmail.com>
2022-10-06 10:35:25 +0400

    cors-anywhere 2

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ var cors_proxy = require('cors-anywhere');
 
 var host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
-var port = process.env.PORT || 1337;
+var port1 = process.env.PORT || 1337;
 
 cors_proxy.createServer({
     originWhitelist: [], // Allow all origins
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
-}).listen(port, host, function() {
-    console.log('Running CORS Anywhere on ' + host + ':' + port);
+}).listen(port1, host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + port1);
 });
 
 const routes = require("./routes");
